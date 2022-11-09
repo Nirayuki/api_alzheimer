81c9c2b405f57ff6a3e1fc719431a6b664524e01
Aron Felippe Kerkhoven <aronkerk8@gmail.com>
2022-11-09 20:41:45 -0300

    Register att

--- routes/register.js
+++ routes/register.js
@@ -11,101 +11,120 @@ router.post('/', (req, res, next) => {
 
     var idUsuario;
 
-    if(tipo_cuidador == 1){
-        mysql.getConnection((error, conn) => {
-            bcrypt.hash(senha, saltRounds, (err, hash) => {
-                console.log(hash)
+    mysql.getConnection((error, conn) => {
+            conn.query(
+                'SELECT * FROM Usuario WHERE Email = ?',
+               [email],
+                (error, resultado, field) => {
     
-                if(err) {
-                    console.log(err)
-                }
+                    conn.release();
+
+                    console.log(resultado.length, resultado);
     
-                conn.query(
-                    'INSERT INTO Usuario (TIPO_CUIDADOR_PACIENTE, Email, Senha) VALUES (?,?,?)',
-                   [tipo_cuidador , email, hash],
-                    (error, resultado, field) => {
-        
-                        conn.release();
-        
-                        if (error) {
-                            res.status(500).send({
-                                error: error,
-                                response: null
-                            });
+                    if(resultado.length == 0) {
+                        if(tipo_cuidador == 1){
+                            mysql.getConnection((error, conn) => {
+                                bcrypt.hash(senha, saltRounds, (err, hash) => {
+                                    console.log(hash)
+                       
+                                    if(err) {
+                                        console.log(err)
+                                    }
+                       
+                                    conn.query(
+                                        'INSERT INTO Usuario (TIPO_CUIDADOR_PACIENTE, Email, Senha) VALUES (?,?,?)',
+                                       [tipo_cuidador , email, hash],
+                                        (error, resultado, field) => {
+                           
+                                            conn.release();
+                           
+                                            if (error) {
+                                               res.status(500).send({
+                                                   error: error,
+                                                   response: null
+                                                });
+                                            }
+                       
+                                            res.status(201).send({
+                                                mensagem: 'Registro feito com sucesso'
+                                            })
+                           
+                                       }
+                                   )
+                           
+                               }) 
+                           })
                         }
-    
-                        res.status(201).send({
-                            mensagem: 'Registro feito com sucesso'
-                        })
-        
+                   
+                   
+                       if(tipo_cuidador == 2){
+                           mysql.getConnection((error, conn) => {
+                               bcrypt.hash(senha, saltRounds, (err, hash) => {
+                       
+                                   if(err) {
+                                       console.log(err)
+                                   }
+                       
+                                   conn.query(
+                                       'INSERT INTO Usuario (TIPO_CUIDADOR_PACIENTE, Email, Senha) VALUES (?,?,?)',
+                                      [tipo_cuidador , email, hash],
+                                       (error, resultado, field) => {
+                           
+                                           conn.release();
+                   
+                                           idUsuario = resultado.insertId
+                   
+                                           console.log(resultado);
+                   
+                                           if (error) {
+                                               res.status(500).send({
+                                                   error: error,
+                                                   response: null
+                                               });
+                                           }
+                       
+                                           // res.status(201).send({
+                                           //     mensagem: 'Registro feito com sucesso'
+                                           // })
+                   
+                                           conn.query(
+                                               'INSERT INTO Cuidador (idUsuario, Nome, Data_Nascimento) VALUES (?,"","00-00-0000")',
+                                              [idUsuario],
+                                               (error, resultado, field) => {
+                                   
+                                                   conn.release();
+                           
+                                                   if (error) {
+                                                       res.status(500).send({
+                                                           error: error,
+                                                           response: null
+                                                       });
+                                                   }
+                               
+                                                   res.status(201).send({
+                                                       mensagem: 'Registro feito com sucesso o cuidador'
+                                                   })
+                                   
+                                               }
+                                           )
+                                   
+                           
+                                       }
+                                   )
+                                  
+                               }) 
+                           })
+                       }
+                    } else {
+                        res.send({message: "Email já existe"});
                     }
-                )
-        
-            }) 
-        })
-    }
-
-
-    if(tipo_cuidador == 2){
-        mysql.getConnection((error, conn) => {
-            bcrypt.hash(senha, saltRounds, (err, hash) => {
     
-                if(err) {
-                    console.log(err)
                 }
+            )
     
-                conn.query(
-                    'INSERT INTO Usuario (TIPO_CUIDADOR_PACIENTE, Email, Senha) VALUES (?,?,?)',
-                   [tipo_cuidador , email, hash],
-                    (error, resultado, field) => {
-        
-                        conn.release();
-
-                        idUsuario = resultado.insertId
-
-                        console.log(resultado);
-
-                        if (error) {
-                            res.status(500).send({
-                                error: error,
-                                response: null
-                            });
-                        }
-    
-                        // res.status(201).send({
-                        //     mensagem: 'Registro feito com sucesso'
-                        // })
-
-                        conn.query(
-                            'INSERT INTO Cuidador (idUsuario, Nome, Data_Nascimento) VALUES (?,"","00-00-0000")',
-                           [idUsuario],
-                            (error, resultado, field) => {
-                
-                                conn.release();
-        
-                                if (error) {
-                                    res.status(500).send({
-                                        error: error,
-                                        response: null
-                                    });
-                                }
-            
-                                res.status(201).send({
-                                    mensagem: 'Registro feito com sucesso o cuidador'
-                                })
-                
-                            }
-                        )
-                
-        
-                    }
-                )
-
-
-               
-            }) 
         })
-    }
+
+     
 
     
 });
