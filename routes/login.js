5eefd543a5c40c5b5f9a6e02b83f020f4adbca8f
Aron Felippe Kerkhoven <aronkerk8@gmail.com>
2022-11-15 14:53:55 -0300

    Update Login

--- routes/login.js
+++ routes/login.js
@@ -4,10 +4,31 @@ const mysql = require('../mysql').pool;
 const bcrypt = require('bcrypt');
 require('dotenv').config()
 
+
+
 router.post('/', (req, res, next) => {
 
     const email = req.body.email;
     const senha = req.body.senha;
+
+    var idUsuario;
+    var idCuidador;
+
+    const data = {
+        idCuidador: 0,
+        idUsuario: 0,
+        TIPO_CUIDADOR_PACIENTE: 0,
+        Email: 0,
+        Senha: 0
+    }
+
+    getData = (resultado, result) => {
+        data.idCuidador = result;
+        data.idUsuario = resultado[0].idUsuario;
+        data.TIPO_CUIDADOR_PACIENTE = resultado[0].TIPO_CUIDADOR_PACIENTE;
+        data.Email = resultado[0].Email;
+        data.Senha = resultado[0].Senha;
+    }
 
     mysql.getConnection((error, conn) => {
         conn.query(
@@ -20,16 +41,32 @@ router.post('/', (req, res, next) => {
                     res.send({ error: error});
                 }
 
+                idUsuario = resultado[0].idUsuario;
+
+                
                 if(resultado.length > 0) {
                     bcrypt.compare(senha, resultado[0].Senha, (error, response) => {
-
-                       
                         if (response) {
-                           //const token = jwt.sign({userId: resultado[0].idUsuario}, process.env.ACESS_TOKEN_SECRET)
-                           console.log(resultado[0])
-                            res.send({data: resultado[0]});
+                            
+                            conn.query(
+                                'SELECT idCuidador FROM Cuidador WHERE idUsuario = ?',
+                                idUsuario,
+                                (error, result, field) => {
+                                    conn.release();
+                                    if (error) {
+                                        res.send({ error: error});
+                                    }
+            
+                                    
+                                    idCuidador = result[0].idCuidador;
+
+                                    getData(resultado, idCuidador);
+            
+                                    res.send(data);
+                                }
+                            ) 
                         } else {
-                            res.send({auth: false, message: "Login Incorreto"});
+                            res.send({message: "Login Incorreto"});
                         }
                     })
                 } else {
